437d46278f78ed161048d7e6add09567486d3a18
Marcus Queiros <marcusdavi.soar@gmail.com>
2022-07-14 11:31:29 -0300

    migration to create address entity

---
+++ src/database/migrations/1657808036611-CreateAddress.ts
@@ -0,0 +1,54 @@
+import { MigrationInterface, QueryRunner, Table } from "typeorm";
+
+export class CreateAddress1657808036611 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    queryRunner.createTable(
+      new Table({
+        name: "address",
+        columns: [
+          {
+            name: 'id',
+            type: 'int',
+            isPrimary: true,
+            generationStrategy: 'increment',
+            isGenerated: true,
+          },
+          {
+            name: 'district',
+            type: 'varchar(255)',
+          },
+          {
+            name: 'street',
+            type: 'varchar(255)',
+          },
+          {
+            name: 'number',
+            type: 'int',
+          },
+          {
+            name: 'complemente',
+            type: 'varchar(255)',
+            isNullable: true,
+          },
+          {
+            name: 'nickname',
+            type: 'varchar(255)',
+            isNullable: true,
+          },
+          {
+            name: 'created_at',
+            type: 'timestamp',
+            default: 'CURRENT_TIMESTAMP',
+          },
+          {
+            name: 'updated_at',
+            type: 'timestamp',
+            default: 'CURRENT_TIMESTAMP',
+          },
+        ],
+      })
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {}
+}
