e47eb58295e536267b6bfdcbb06af0aa25b4020a
Marcus Queiros <marcusdavi.soar@gmail.com>
2022-07-14 12:26:49 -0300

    adding addres model

---
+++ src/models/address.ts
@@ -0,0 +1,43 @@
+import { Column, CreateDateColumn, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
+
+export class address {
+  @PrimaryGeneratedColumn('increment')
+  id: number;
+
+  @Column({ length: 255 })
+  district: string;
+
+  @Column({ length: 255 })
+  street: string;
+
+  @Column()
+  number: number;
+
+  @Column({ nullable: true })
+  complement: string;
+
+  @Column({ nullable: true })
+  nickname: string;
+
+  @CreateDateColumn({ name: "created_at" })
+  createdAt: number;
+
+  @UpdateDateColumn({ name: "updated_at" })
+  updatedAt: number;
+
+  constructor(
+    id: number,
+    district: string,
+    street: string,
+    number: number,
+    complement?: string,
+    nickname?: string
+  ) {
+    this.id = id;
+    this.district = district;
+    this.street = street;
+    this.number = number;
+    this.complement = complement;
+    this.nickname = nickname;
+  }
+}
