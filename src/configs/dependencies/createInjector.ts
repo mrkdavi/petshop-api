8af082046bf404144bf5b8a5c086601e80b0b071
Marcus Queiros <marcusdavi.soar@gmail.com>
2022-08-30 16:42:47 -0300

    Updating configs

--- src/configs/dependencies/createInjector.ts
+++ src/configs/dependencies/createInjector.ts
@@ -1,19 +1,27 @@
-import { getCustomRepository } from "typeorm";
 import Container from "typedi";
+import { getCustomRepository } from "typeorm";
 import { PetRepository } from "../../repositories/PetRepository";
+import { AddressRepository } from "../../repositories/AddressRepository";
+import { UserRepository } from "../../repositories/UserRepository";
+import { ActivityRepository } from "../../repositories/ActivityRepository";
+import { AdoptionRepository } from "../../repositories/AdoptionRepository";
 
 // controllers
 import "../../controllers/PetController";
+import "../../controllers/UserController";
 import "../../controllers/AddressController";
 
 // services
 import "../../services/PetService";
+import "../../services/UserService";
 import "../../services/AddressService";
-import { AddressRepository } from "../../repositories/AddressRepository";
 
 const createDependencyInjector = () => {
   Container.set("PetRepository", getCustomRepository(PetRepository));
+  Container.set("UserRepository", getCustomRepository(UserRepository));
   Container.set("AddressRepository", getCustomRepository(AddressRepository));
+  Container.set("ActivityRepository", getCustomRepository(ActivityRepository));
+  Container.set("AdoptionRepository", getCustomRepository(AdoptionRepository));
 };
 
 export default createDependencyInjector;
